126b8d90c2f01aff6860b1805421914a7a61d45f
Santosh Singh <singhsantosh.edu.in@gmail.com>
2022-12-03 18:14:47 +0530

    update script

--- script.sh
+++ script.sh
@@ -1,40 +1,50 @@
-# Reset
-Color_Off='\033[0m'       # Text Reset
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
 
+# Succeeded
+IsDockerLoginSucceeded='\033[1;92m========== 1 -Login Succeeded. ==========\033[1;92m'
+IsDockerBuildSuccessded='\033[1;92m========== 2 -Docker Build Succeeded. ==========\033[1;92m'
+IsDockerBuildTagSuccessded='\033[1;92m========== 3 -Docker Build Completed Tag Succeeded. ==========\033[1;92m'
+IsDockerBuildPushSuccessded='\033[1;92m========== 4 -Docker Build Push Succeeded. ==========\033[1;92m'
+# Failed
+IsDockerLoginFailed='\033[1;91m********** 1 -Login Failed. **********\033[1;91m'
+IsDockerBuildFailed='\033[1;91m********** 2 -Docker Build Failed. **********\033[1;91m'
+IsDockerBuildTagFailed='\033[1;91m********** 3 -Docker Build Tag Failed. **********\033[1;91m'
+IsDockerBuildPushFailed='\033[1;91m********** 4 -Docker Build Push Failed. **********\033[1;91m'
+
+# Execute Docker Command one By one
 if aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/x8d1s6y8 | grep -q "Login Succeeded"
   then
-    echo "${BGreen}========== 1 - Login Succeeded. ==========${BGreen}"
+    echo "${IsDockerLoginSucceeded}"
       if docker build -t script-app .
         then
-          echo "${BGreen}========== 2 - Docker Build Succeeded. ==========${BGreen}"
+          echo "${IsDockerBuildSuccessded}"
             if docker tag script-app:latest public.ecr.aws/x8d1s6y8/script-app:latest
               then
-                echo "${BGreen}========== 3 - Docker Build Completed Tag Succeeded. ==========${BGreen}"
+                echo "${IsDockerBuildTagSuccessded}"
                   if docker push public.ecr.aws/x8d1s6y8/script-app:latest
                     then
-                      echo "${BGreen}========== 4 - Docker Build Push Succeeded. ==========${BGreen}"
+                      echo "${IsDockerBuildPushSuccessded}"
                     else
-                      echo "${BRed}********** 4- Docker Build Push Failed. **********${BRed}"
+                      echo "${IsDockerBuildPushFailed}"
                       exit
                   fi
               else
-                echo "${BRed}********** 3- Docker Build Tag Failed. **********${BRed}"
+                echo "${IsDockerBuildTagFailed}"
                 exit
             fi
         else
-          echo "${BRed}********** 2- Docker Build Failed. **********${BRed}"
+          echo "${IsDockerBuildFailed}"
           exit
       fi
   else
-    echo "${BRed}********** 1- Login Failed. **********${BRed}"
+    echo "${IsDockerLoginFailed}"
     exit
 fi
